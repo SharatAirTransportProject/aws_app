cd9d5eb225b57ca9c656b5036f081a39a0b797a5
SharatAirTransportProject <sg2232@jagmail.southalabama.edu>
2024-05-22 11:34:56 -0500

    fifth push

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ from flask import Flask
 import psycopg2
 
 #RDS connection parameters
-ENDPOINT= "studentdb.cm8epoorygw1.us-east-1.rds.amazonaws.com"
+ENDPOINT= "database-1.cvi40q4igbd6.us-east-2.rds.amazonaws.com"
 PORT= "5432"
 USER= "postgres"
-PASSWORD = "Student11"
+PASSWORD = "Student1"
 REGION= "us-east-1"
 DBNAME= "postgres"
 
@@ -22,7 +22,7 @@ def get_students():
     try:
         conn = psycopg2.connect(host=ENDPOINT, port=PORT, database=DBNAME, user=USER, password=PASSWORD)
         cur = conn.cursor()
-        cur.execute("""SELECT * from test""")
+        cur.execute("""SELECT * from student""")
         query_results = cur.fetchall()
         print(query_results)
         return(query_results)
